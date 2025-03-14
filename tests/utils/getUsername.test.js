bb93cf956674f92d8970e84b0c3860a574e1717a
Stine Knutsen <160833015+stineknutsen@users.noreply.github.com>
2025-03-14 22:02:58 +0100

    write test for getUserName

---
+++ tests/utils/getUsername.test.js
@@ -0,0 +1,20 @@
+import { getUsername } from "../../js/utils/storage.js";
+import { describe, it, expect, beforeEach } from "vitest";
+
+describe("getUsername", () => {
+  beforeEach(() => {
+    localStorage.clear();
+  });
+
+  // test 1: returns the name from the user object in storage (first save user object to storage)
+  it("returns the name from the user object in storage", () => {
+    const user = { name: "Peter Pan" };
+    localStorage.setItem("user", JSON.stringify(user));
+    expect(getUsername()).toBe("Peter Pan");
+  });
+
+  // test 2: returns null when no user exists in storage
+  it("returns null when no user exists in storage", () => {
+    expect(getUsername()).toBe(null);
+  });
+});
