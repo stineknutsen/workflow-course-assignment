cb6c918c590ca84f7b7b23575186784fe8b3d8ab
Stine Knutsen <160833015+stineknutsen@users.noreply.github.com>
2025-03-16 21:43:29 +0100

    fix path

--- tests/listeners/auth/loginFormListener.spec.js
+++ tests/listeners/auth/loginFormListener.spec.js
@@ -2,7 +2,7 @@ import { test, expect } from "@playwright/test";
 
 test.describe("login", () => {
   test("user can log in with valid credentials", async ({ page }) => {
-    await page.goto("/auth/login");
+    await page.goto("/login");
     await page.locator('input[name="email"]').fill(process.env.TEST_USER_EMAIL);
     await page
       .locator('input[name="password"]')
@@ -15,7 +15,7 @@ test.describe("login", () => {
   test("an error message is shown with invalid credentials", async ({
     page,
   }) => {
-    await page.goto("/auth/login");
+    await page.goto("/login");
     await page.locator('input[name="email"]').fill(process.env.TEST_USER_EMAIL);
     await page.locator('input[name="password"]').fill("lijihoppomp");
     await page.getByRole("button", { name: "Login" }).click();
