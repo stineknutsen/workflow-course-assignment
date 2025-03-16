efe8c577f78e205cc4aeec585169d965393bf957
Stine Knutsen <160833015+stineknutsen@users.noreply.github.com>
2025-03-16 22:11:58 +0100

    remove notes from vitest tests

--- js/utils/getUsername.test.js
+++ js/utils/getUsername.test.js
@@ -6,14 +6,12 @@ describe("getUsername", () => {
     localStorage.clear();
   });
 
-  // test 1: returns the name from the user object in storage (first save user object to storage)
   it("returns the name from the user object in storage", () => {
     const user = { name: "Peter Pan" };
     localStorage.setItem("user", JSON.stringify(user));
     expect(getUsername()).toBe("Peter Pan");
   });
 
-  // test 2: returns null when no user exists in storage
   it("returns null when no user exists in storage", () => {
     expect(getUsername()).toBe(null);
   });
--- js/utils/isActivePath.test.js
+++ js/utils/isActivePath.test.js
@@ -2,28 +2,24 @@ import { isActivePath } from "./userInterface.js";
 import { describe, it, expect } from "vitest";
 
 describe("isActivePath", () => {
-  // test 1: return true when current path matches href exactly
   it("returns true when current path matches href exactly", () => {
     const href = "/login";
     const currentPath = "/login";
     expect(isActivePath(href, currentPath)).toBe(true);
   });
 
-  // test 2: return true for root path ("/") when path is "/" or " "/index.html"
   it("returns true for root path ('/') when path is '/' or '/index.html'", () => {
     const href = "/";
     const currentPath = "/index.html";
     expect(isActivePath(href, currentPath)).toBe(true);
   });
 
-  // test 3: return true when current path includes href
   it("returns true when current path includes href", () => {
     const href = "/venue";
     const currentPath = "/venue/?id=1";
     expect(isActivePath(href, currentPath)).toBe(true);
   });
 
-  // test 4: return false when paths don't match
   it("returns false when paths don't match", () => {
     const href = "/login";
     const currentPath = "/register";
